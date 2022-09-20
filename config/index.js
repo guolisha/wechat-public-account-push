437c054b8f42b83d8567d99d684b59a51479c564
guolisha <113963429+guolisha@users.noreply.github.com>
2022-09-20 18:58:59 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -64,7 +64,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "o0f0T51KVnjAJDW5Ej0vFJ4p46vk",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "6V6YhRWUQb26r-gISvfHbahfcCW4uzuxTAf3YcMJYxw",
+      useTemplateId: "q8pDoMj2X1rLTPAmcol8EkC71aOn66GQLxnYJ4MM-To",
       // 所在省份
       province: "北京",
       // 所在城市
